2d1a35df18868b94a8315cea768e7ca80c66d0a6
EsheelAnand1 <esheelroy@gmail.com>
2017-08-11 18:09:27 +1200

    InitetCommit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       // User start program
+            // Uer enters code Km to miles or miles to km
+            // user types data
+            // gets anwser on screen
+            //end
+           do
+             {
+                 Console.WriteLine("Convert Km to miles or Miles to Km");
+
+
+                 Console.WriteLine("Choose convertion below");
+                 Console.WriteLine("KM to Miles          Code KM");
+                 Console.WriteLine("Miles to Km          Code Miles");
+                 Console.WriteLine("Exit                 Code Exit");
+                 string Kilometer = "KM";
+                 string Miles = "Miles";
+                 string Exit = "Exit";
+
+                 string userInput = Console.ReadLine();
+
+
+                 if (userInput == Kilometer)
+                 {
+
+
+                     Console.WriteLine("Enter KM");
+                     Double KM = Double.Parse(Console.ReadLine());
+                     Double Convertednumber = Math.Round(KM * 0.62137119, 2);
+                     Console.WriteLine($"{Convertednumber} Milles");
+                 }
+
+                 else if (userInput == Miles)
+                 {
+                     Console.WriteLine("Enter Miles");
+                     Double Ml = Double.Parse(Console.ReadLine());
+                     Double Convertednumber = Math.Round(Ml * 1.609344, 2);
+                     Console.WriteLine($"{Convertednumber} KM");
+                 }
+
+                 else if (userInput == Exit)
+                 {
+                     Environment.Exit(1);
+                 }
+
+                 else
+                 {
+                     Console.WriteLine("Error you didn't enter number or code");
+                 }
+
+
+                 Console.ReadLine();
+             } while (KM == userInput); 
+
+            
+              
+              for (int i = 1; i <= 5; i++)
+              {
+                Console.WriteLine("What is Your fave fruit!");
+                var userInput = Console.ReadLine();
+                
+              } Console.WriteLine($" {i + userInput}");
+            Console.ReadLine();
+
+        }
+    }
+}
